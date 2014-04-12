4732c74a546c588ec797a8507909585e89df4f45
KRtekTM <ok@krtkovo.eu>
2014-04-12 03:38:39 +0200

    Version 3.0.3 dev

--- www/.config - Copy.php
+++
@@ -1,6 +0,0 @@
-<?php
-$ktsDbHost="";      //name of MySQL server (this computer=localhost)
-$ktsDbUser="";        //user name for connection to database
-$ktsDbPasswd="";        //password for user
-$ktsDbName="";    //database where are tables of KTS
-?>
