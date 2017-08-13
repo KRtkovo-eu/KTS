484d0b2d8b6126b64f6084563ee1a8f9602191a5
Ondřej Kotas <ok@krtkovo.eu>
2017-08-13 19:12:19 +0200

    v3.0.4

--- www/admin/pages/config.php
+++ www/admin/pages/config.php
@@ -1,31 +1,6 @@
-﻿<?php
+<?php
 $tproc->set("title", $db->QueryValue("value","kts_config","name='title'"));
 $tproc->set("description", $db->QueryValue("value","kts_config","name='description'"));
 $tproc->set("keywords", $db->QueryValue("value","kts_config","name='keywords'"));
 
-require("common.php");
-
-$ktsNewVersion = stream_get_contents(fopen($ktsVersionURL, "r"));  
-$ktsUpdateLink = stream_get_contents(fopen($ktsUpdateURL, "r"));   
-
-if($ktsNewVersion > $ktsVersion) {
-  $tproc->set("updateDiv", "available");
-  $tproc->set("updateComment", "Aktualizace je k dispozici");
-  $tproc->set("updateButton", "visible");
-  $tproc->set("updateLink", $ktsUpdateLink);
-}
-elseif($ktsNewVersion == $ktsVersion) {
-  $tproc->set("updateDiv", "success");
-  $tproc->set("updateComment", "Máte aktuální verzi");
-  $tproc->set("updateButton", "hidden");
-}
-else {
-  $tproc->set("updateDiv", "wrong");
-  $tproc->set("updateComment", "Instalace není standardní");
-  $tproc->set("updateButton", "visible");
-  $tproc->set("updateLink", $ktsUpdateLink);
-}
-
-$tproc->set("updateVersion", $ktsNewVersion);
-$tproc->set("currentVersion", $ktsVersion);
 ?>
